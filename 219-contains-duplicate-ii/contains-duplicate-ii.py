42e360163da021b82ee2fc9003a8bc36304ff902
muku2006nth <155838594+muku2006nth@users.noreply.github.com>
2025-08-06 10:37:20 +0530

    Time: 56 ms (24.52%) | Memory: 25.6 MB (28.16%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def containsNearbyDuplicate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        seen=set()
+        for i in range (len(nums)):
+            if nums[i] in seen:
+                return True
+            seen.add(nums[i])   
+            
+            if len(seen)>k:
+                seen.remove(nums[i-k])
+        return False    
+
+            
