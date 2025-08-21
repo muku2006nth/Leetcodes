eeb76b15e6e2d049e7230888c7aaba5a3c064567
muku2006nth <155838594+muku2006nth@users.noreply.github.com>
2025-08-21 22:12:55 +0530

    Time: 8 ms (21.55%) | Memory: 12.4 MB (85.73%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        result = []
+        carry = 0
+
+        max_len = max(len(a), len(b))
+        a = a.zfill(max_len)
+        b = b.zfill(max_len)
+
+
+        for i in range(max_len - 1, -1, -1):
+            bit_a = int(a[i])
+            bit_b = int(b[i])
+
+            total = bit_a + bit_b + carry
+            carry = total // 2
+            result.append(str(total % 2))
+
+        if carry:
+            result.append('1')
+
+
+        return ''.join(reversed(result))
