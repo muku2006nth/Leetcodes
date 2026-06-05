95474c847ef4a6d7b1a1e68fb2371aad51ff21bf
Mukunth Gopi <155838594+muku2006nth@users.noreply.github.com>
2026-06-05 19:50:48 +0530

    Time: 16 ms (10.45%) | Memory: 21.3 MB (20.49%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,20 +1,12 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        count = 0
-        candidate = None
-
-        for num in nums:
-            if count == 0:
-                candidate = num
-                count = 1
-            elif num == candidate:
-                count += 1
-            else:
-                count -= 1
-
-        return candidate
-
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        hash = {}
+        res = majority = 0
+        
+        for n in nums:
+            hash[n] = 1 + hash.get(n, 0)
+            if hash[n] > majority:
+                res = n
+                majority = hash[n]
+        
+        return res
